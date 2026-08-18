5486c888400d35c0ce912d9bd890bcb57ec0804b
Biza Munya <bizamunya@gmail.com>
2026-08-18 14:42:52 +0200

    added more notes in component communication

---
+++ projects/tracker/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+
+function MovieTracker() {
+  return (
+    <div>
+      <h1>Movie Tracker</h1>
+    </div>
+  );
+
+  function MovieForm() {
+const [state, setstate] = useState(initialState)    
+    return(
+      <div>
+
+      </div>
+    )
+  }
+
+}
+function App() {
+  return (
+    <div>
+      <MovieTracker />
+    </div>
+  );
+}
+
+export default App;
